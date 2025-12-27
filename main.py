964158d45c6f9b98f5b7df4e8641b9c7075024ad
Roman <Ramenm1001@gmail.com>
2025-12-27 22:34:02 +1000

    func изменено4

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ def func(x):
         x += 1
     return x
 
+def func2(x):
+    x = x ** 2
+    for i in range(25):
+        x += 1
+    return x
+
+def func3(z):
+    z= z ** 2
+    for i in range(25):
+        z += 1
+    return z
 
 print("hello world")
 
